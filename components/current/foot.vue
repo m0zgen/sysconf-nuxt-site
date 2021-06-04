17db0c136dba846827c4b08505d2de6dca5aef22
/:) <g.yevgeniy.p@gmail.com>
2021-06-04 11:31:59 +0300

    second commit

--- components/current/foot.vue
+++ components/current/foot.vue
@@ -9,8 +9,11 @@
             <a class="level-item" href="https://t.me/OpenSysConf" target="_blank">SysConf Group</a>
           </div>
           <div class="level">
-            <a href="https://t.me/b4tr_channel" target="_blank">@b4tr_channel</a>
+            <a href="https://t.me/b4tr_channel" target="_blank">b4tr_channel</a>
             <a href="https://t.me/OrderOfSixAngles" target="_blank">Order of Six Angles</a>
+          </div>
+          <div class="level">
+            <a href="https://t.me/novitoll_ch" target="_blank">novitoll_ch</a>
           </div>
         </div>
         <div class="column is-4 has-text-centered is-hidden-mobile orange is-bold">
@@ -24,6 +27,10 @@
           <div class="level">
             <a href="https://hackervision.org/" target="_blank">Hackervision.org</a>
             <a href="https://t.me/mikrotikninja" target="_blank">Mikrotik Ninja</a>
+          </div>
+          <div class="level">
+            <a href="#" target="_blank"></a>
+            <a href="https://btsdigital.kz/en/" target="_blank">Btsdigital.kz</a>
           </div>
         </div>
       </div>
