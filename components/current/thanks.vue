805ce689138b1b2f5295303f9c552e194fac3c73
/:) <g.yevgeniy.p@gmail.com>
2021-06-05 09:24:57 +0300

    update thanks

--- components/current/thanks.vue
+++ components/current/thanks.vue
@@ -3,7 +3,7 @@
     <div class="container has-text-centered">
       <h2 class="title is-spaced white">Благодарности</h2>
       <div class="block orange divide">~ ~ ~</div>
-
+      <h3 class="subtitle white">Организационная и финансовая помощь</h3>
       <ul class="white">
         <li v-for="(user, index) in content.users">
 <!--          <div v-if="index == content.users.length - 1">;</div>-->
@@ -11,7 +11,47 @@
          - {{user.name}}
         </li>
       </ul>
+      <br>
+      <h3 class="subtitle white">Информационная поддержка</h3>
 
+      <div class="columns">
+        <div class="column is-4 has-text-centered">
+          <div class="level">
+            <a class="level-item" href="https://t.me/OpenSysConf" target="_blank">SysConf Group</a>
+          </div>
+          <div class="level">
+            <a href="#"></a>
+            <a href="https://t.me/b4tr_channel" target="_blank">b4tr_channel</a>
+          </div>
+          <div class="level">
+
+          </div>
+        </div>
+        <div class="column is-4 has-text-centered is-hidden-mobile orange is-bold">
+          <a class="level-item" href="https://t.me/sysadm_in_channel" target="_blank">Sys-Admin Channel</a>
+        </div>
+        <div class="column is-4 has-text-centered">
+          <div class="level">
+            <a class="level-item" href="https://t.me/sysadm_in" target="_blank">Sys-Admin Help</a>
+          </div>
+          <div class="level">
+            <a href="https://t.me/mikrotikninja" target="_blank">Mikrotik Ninja</a>
+          </div>
+          <div class="level">
+            <a href="#" target="_blank"></a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+      <h3 class="subtitle white">Дружественные ресурсы</h3> |
+      <a href="https://nitroteam.kz/" target="_blank">Nitro Team</a> |
+      <a href="https://spectre.kz/" target="_blank">Spectre Security</a> |
+      <a href="https://btsdigital.kz/" target="_blank">BTS Digital</a> |
+      <a href="https://hackervision.org/" target="_blank">Hacker Vision</a> |
+      <a href="https://sys-adm.in">Sys-Adm.in</a> |
+      <a href="https://t.me/OrderOfSixAngles" target="_blank">Order of Six Angles</a> |
+      <a href="https://t.me/novitoll_ch" target="_blank">novitoll_ch</a> |
     </div>
   </section>
 </template>
