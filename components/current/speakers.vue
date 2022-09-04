14f901e803bbd015105c30f832a81014183fc033
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-09-04 15:03:35 +0200

    + is-box-100, sheldon announce ready

--- components/current/speakers.vue
+++ components/current/speakers.vue
@@ -34,112 +34,113 @@
                     <p class="mb-1">Доклад:</p>
                     <p><strong>Практический опыт с 4G, 5G</strong></p>
                     <p>
-                      Кодим Rx, Tx функционалы на С, описывая теорию (OFDM/SC-OFDM демодуляция, логические и транспорт каналы 4G, 5G), подводные камни имплементации. Анализируем трафик.
+                      Кодим Rx, Tx функционалы на С, описывая теорию (OFDM/SC-OFDM демодуляция, логические и транспорт каналы 4G, 5G), подводные камни имплементации.
+                      Анализируем трафик.
                     </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5">
-                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_kunkg_lao.jpeg" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      Kutlymurat "manfromkz" Mambetniyazov
-                    </h4>
-                    <p class="mb-4 has-text-grey">Senior Security Testing Engineer | OSCP | eWPTXv2 | eCPTXv2 | Master of Computer Sciencer</p>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>История одного CVE</strong></p>
-                    <p>Рассказ о том, как простой пентест веб-приложения привел к выявлению SSRF-уязвимости в популярном менеджере рабочего стола для Linux</p>
-                    <br>
-                  </div>
-                </div>
-              </div>
-            </div>
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5">-->
+<!--                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_kunkg_lao.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      Kutlymurat "manfromkz" Mambetniyazov-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">Senior Security Testing Engineer | OSCP | eWPTXv2 | eCPTXv2 | Master of Computer Sciencer</p>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>История одного CVE</strong></p>-->
+<!--                    <p>Рассказ о том, как простой пентест веб-приложения привел к выявлению SSRF-уязвимости в популярном менеджере рабочего стола для Linux</p>-->
+<!--                    <br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
             <!--            Section-->
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5">
-                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/speaker_22_t2micro_.jpeg" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      Ilyas B Arinov "t2micro"
-                    </h4>
-                    <p class="mb-4 has-text-grey">DevOps</p>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>Старый... Но не бесполезный</strong></p>
-                    <p>Применение протокола IRC в современной инфраструктуре для построения сервис-ориентированной модели алертинга.</p>
-                    <br><br>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5">
-                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_noob.jpeg" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      (vacancy place)
-                    </h4>
-                    <p class="mb-4 has-text-grey">(vacancy place)</p><br>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>(vacancy place)</strong></p>
-                    <p>(vacancy place)</p><br><br><br><br>
-                    <br>
-                  </div>
-                </div>
-              </div>
-            </div>
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5">-->
+<!--                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/speaker_22_t2micro_.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      Ilyas B Arinov "t2micro"-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">DevOps</p>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>Старый... Но не бесполезный</strong></p>-->
+<!--                    <p>Применение протокола IRC в современной инфраструктуре для построения сервис-ориентированной модели алертинга.</p>-->
+<!--                    <br><br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5">-->
+<!--                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_noob.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      (vacancy place)-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">(vacancy place)</p><br>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>(vacancy place)</strong></p>-->
+<!--                    <p>(vacancy place)</p><br><br><br><br>-->
+<!--                    <br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            Section-->
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5">-->
+<!--                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_scorpion.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      Roman "sysroman"-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">Ведущий Linux сисадмин в Билайн Казахстан</p>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>Ansible AWX by Oracle</strong></p>-->
+<!--                    <p>Рассказ на примерах про веб-приложение с открытым исходным кодом от Oracle, представляющее пользовательский интерфейс, REST API и механизм задач для Ansible</p>-->
+<!--                    <br><br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5">-->
+<!--                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_batman.png" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      Дмитрий "ватман"-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">Админ, архитектор</p><br>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>CDC</strong></p>-->
+<!--                    <p>CDC и как узнавать об изменении данных раньше всех, ну почти.</p><br><br><br><br>-->
+<!--                    <br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
 <!--            Section-->
             <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5">
-                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_scorpion.jpeg" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      Roman "sysroman"
-                    </h4>
-                    <p class="mb-4 has-text-grey">Ведущий Linux сисадмин в Билайн Казахстан</p>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>Ansible AWX by Oracle</strong></p>
-                    <p>Рассказ на примерах про веб-приложение с открытым исходным кодом от Oracle, представляющее пользовательский интерфейс, REST API и механизм задач для Ansible</p>
-                    <br><br>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5">
-                    <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_batman.png" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      Дмитрий "ватман"
-                    </h4>
-                    <p class="mb-4 has-text-grey">Админ, архитектор</p><br>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>CDC</strong></p>
-                    <p>CDC и как узнавать об изменении данных раньше всех, ну почти.</p><br><br><br><br>
-                    <br>
-                  </div>
-                </div>
-              </div>
-            </div>
-<!--            Section-->
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
+              <div class="box has-background-light is-box-100">
                 <div class="is-vcentered columns is-multiline">
                   <div class="column is-5">
                     <img class="image is-fullwidth" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_smoke2.jpeg" alt="">
@@ -148,6 +149,7 @@
                     <h4 class="mb-2 is-size-4 has-text-weight-bold">
                       Shelldon "Sh3lldon"
                     </h4>
+                    <br>
                     <p class="mb-4 has-text-grey">Reverse engineer - binary exploitation expert Windows Kernel/Driver/User Mode Exploit Developer. Студент 3 курса. </p>
                     <p class="mb-1">Доклад:</p>
                     <p><strong>DEP vs ROP</strong></p>
@@ -158,28 +160,28 @@
                 </div>
               </div>
             </div>
-            <div class="column is-6 mb-4">
-              <div class="box has-background-light">
-                <div class="is-vcentered columns is-multiline">
-                  <div class="column is-5 is-hidden-mobile">
-                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_liu_kang.jpeg" alt="">
-                  </div>
-                  <div class="column is-5 is-hidden-desktop is-hidden-tablet">
-                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_noghtwolf.jpeg" alt="">
-                  </div>
-                  <div class="column is-7">
-                    <h4 class="mb-2 is-size-4 has-text-weight-bold">
-                      Yevgeniy "sysadminkz"
-                    </h4>
-                    <p class="mb-4 has-text-grey">IT/IS Awareness Evangelist | Open BLD DNS Service Founder and more...</p>
-                    <p class="mb-1">Доклад:</p>
-                    <p><strong>Нейромаркетинг Awareness</strong></p>
-                    <p>Нейромаркетинг - создание действия до того, как человек осознал чей это дейтвительно был выбор. Рассказ о том, как была построена система неявной превентивной защиты, на примере Open Source проекта Open BLD DNS</p>
-                    <br>
-                  </div>
-                </div>
-              </div>
-            </div>
+<!--            <div class="column is-6 mb-4">-->
+<!--              <div class="box has-background-light">-->
+<!--                <div class="is-vcentered columns is-multiline">-->
+<!--                  <div class="column is-5 is-hidden-mobile">-->
+<!--                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_liu_kang.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-5 is-hidden-desktop is-hidden-tablet">-->
+<!--                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_noghtwolf.jpeg" alt="">-->
+<!--                  </div>-->
+<!--                  <div class="column is-7">-->
+<!--                    <h4 class="mb-2 is-size-4 has-text-weight-bold">-->
+<!--                      Yevgeniy "sysadminkz"-->
+<!--                    </h4>-->
+<!--                    <p class="mb-4 has-text-grey">IT/IS Awareness Evangelist | Open BLD DNS Service Founder and more...</p>-->
+<!--                    <p class="mb-1">Доклад:</p>-->
+<!--                    <p><strong>Нейромаркетинг Awareness</strong></p>-->
+<!--                    <p>Нейромаркетинг - создание действия до того, как человек осознал чей это дейтвительно был выбор. Рассказ о том, как была построена система неявной превентивной защиты, на примере Open Source проекта Open BLD DNS</p>-->
+<!--                    <br>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
           </div>
         </div>
       </div>
@@ -189,9 +191,6 @@
     <!--      {{user.name}}-->
     <!--    </div>-->
   </section>
-
-
-
 
 </template>
 
