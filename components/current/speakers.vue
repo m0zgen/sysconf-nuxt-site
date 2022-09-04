a344a72b23c386f66993f5fc423db829d70e9ed1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-09-04 08:07:03 +0200

    ~ photo sysadminkz is-mobile/desktop

--- components/current/speakers.vue
+++ components/current/speakers.vue
@@ -123,10 +123,10 @@
               <div class="box has-background-light">
                 <div class="is-vcentered columns is-multiline">
                   <div class="column is-5 is-hidden-mobile">
-                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/speaker_22_sysadminkz_.jpeg" alt="">
+                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_liu_kang.jpeg" alt="">
                   </div>
                   <div class="column is-5 is-hidden-desktop is-hidden-tablet">
-                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_liu_kang.jpeg" alt="">
+                    <img class="image is-fullwidth is-desktop" style="height: 330px; object-fit: cover;" src="~/assets/images-22/ninja_splash_noghtwolf.jpeg" alt="">
                   </div>
                   <div class="column is-7">
                     <h4 class="mb-2 is-size-4 has-text-weight-bold">
