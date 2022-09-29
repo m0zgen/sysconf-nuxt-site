29b71cd263178ec731cd4742d9b54bec8aab39e9
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-09-29 12:27:06 +0200

    + disable speaker registration

--- components/current/register.vue
+++ components/current/register.vue
@@ -9,41 +9,30 @@
         Участвуй, знакомься, общайся:
       </p>
       <div class="container page-container">
-        <div class="columns">
-          <div class="column has-text-right">
+        <div class="column has-text-centered">
 
-            <a class="button is-dark" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfhKpEC_a8Ze4x2cNr42zjwU0726_9p4irLefLXQtTK53TIGg/viewform?usp=sf_link">
-<!--            <a class="button is-primary" @click='toggleOfflineRegister = !toggleOfflineRegister'>-->
+          <a class="button is-dark" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfhKpEC_a8Ze4x2cNr42zjwU0726_9p4irLefLXQtTK53TIGg/viewform?usp=sf_link">
+            <!--            <a class="button is-primary" @click='toggleOfflineRegister = !toggleOfflineRegister'>-->
             Регистрация участника</a>
 
-          </div>
-          <div class="is-divider-vertical" data-content="-"></div>
-          <div class="column has-text-left">
-            <a class="button is-dark" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeA7liVA4rfdB-OpoGTohTWKIxBN1gkOYdHns7620MsyrpEkw/viewform?usp=sf_link">
-<!--            <a class="button is-primary" @click='toggleSpeakerRegister = !toggleSpeakerRegister'>-->
-              Регистрация докладчика</a>
-          </div>
         </div>
+<!--        <div class="columns">-->
+<!--          <div class="column has-text-right">-->
+
+<!--            <a class="button is-dark" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfhKpEC_a8Ze4x2cNr42zjwU0726_9p4irLefLXQtTK53TIGg/viewform?usp=sf_link">-->
+<!--&lt;!&ndash;            <a class="button is-primary" @click='toggleOfflineRegister = !toggleOfflineRegister'>&ndash;&gt;-->
+<!--            Регистрация участника</a>-->
+
+<!--          </div>-->
+<!--          <div class="is-divider-vertical" data-content="-"></div>-->
+<!--          <div class="column has-text-left">-->
+<!--            <a class="button is-dark" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeA7liVA4rfdB-OpoGTohTWKIxBN1gkOYdHns7620MsyrpEkw/viewform?usp=sf_link">-->
+<!--&lt;!&ndash;            <a class="button is-primary" @click='toggleSpeakerRegister = !toggleSpeakerRegister'>&ndash;&gt;-->
+<!--              Регистрация докладчика</a>-->
+<!--          </div>-->
+<!--        </div>-->
       </div>
 
-<!--      <a class="button is-dark" @click='toggle = !toggle'>Еще немного информации</a>-->
-<!--      <br><br>-->
-<!--      <div v-show='toggleSpeakerRegister'>-->
-<!--        <div class="notification is-warning">-->
-<!--          Регистрация докладчика <strong>закрыта</strong>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <div v-show='toggleOfflineRegister'>-->
-<!--        <div class="notification is-warning">-->
-<!--          Регистрация участников <strong>закрыта</strong>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <div v-show='toggle'>-->
-<!--        <div class="notification is-warning">-->
-<!--          Вопросы можно задавать в <a class="is-ghost" href="https://t.me/OpenSysConf" target="_blank">Open SysConf</a> чате-->
-<!--        </div>-->
-<!--      </div>-->
-<!--&lt;!&ndash;      <a class="button is-warning" onmouseover="glow()" onclick="start()">TEST</a>&ndash;&gt;-->
     </div>
   </section>
 </template>
