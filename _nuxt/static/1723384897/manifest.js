564b9eb1da19251f143507833d7a4f7720ff90ae
m0zgen <g.yevgeniy.p@gmail.com>
2024-08-11 14:01:38 +0000

    Deploying to gh-pages from @ m0zgen/sysconf-nuxt-site@2aa007a14a4194dd66447ad5e4339720c07df6d3 🚀

---
+++ _nuxt/static/1723384897/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmembership","\u002Fabout","\u002Fsysconf-19","\u002Fsysconf-21"]})
---
+++ _nuxt/static/1723384897/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsysconf-nuxt-site\u002F_nuxt\u002Fstatic\u002F1723384897",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsysconf-nuxt-site\u002F",assetsPath:"\u002Fsysconf-nuxt-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
